e6f5d2fb113a84c1e14a8b4abdf0c0b543b8e52e
Josh Parker <joshBBparker@gmail.com>
2020-11-11 21:36:08 -0700

    try permutation solution

--- src/solvers.js
+++ src/solvers.js
@@ -84,11 +84,103 @@ window.findNQueensSolution = function (n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 
+window.permute = function (permutation) {
+  var length = permutation.length;
+  var result = [permutation.slice()];
+  var c = new Array(length).fill(0);
+  var i = 1;
+  var k, p;
+
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i];
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      result.push(permutation.slice());
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+  return result;
+};
+
 window.countNQueensSolutions = function (n) {
   if (n < 2) {
     return 1;
   }
+  var permutations = permute(_.range(n));
+  var allSolutions = [];
 
+  //p[i] != (p[j] - (i-j)) and p[i] != (p[j] - (j-i)
+  for (let k = 0; k < permutations.length; k++) {
+    if (_.every(permutations[k], function (val, i) {
+      for (let j = (i + 1); j < n; j++) {
+        if (val === (permutations[k][j] - (i - j)) || val === (permutations[k][j] - (j - i))) {
+          return false;
+        }
+      }
+      return true;
+    })) {
+      allSolutions.push(permutations[k]);
+    }
+  }
+  solutionCount = allSolutions.length;
+
+  //len([p for p in permutations(range(n)) if all([p[i] != (p[j] - (i-j)) and p[i] != (p[j] - (j-i)) for i in range(n) for j in range(i+1,n)])])
+  //[p for p in permutations(range(n)) if all([p[i] != (p[j] - (i-j)) and p[i] != (p[j] - (j-i)) for i in range(n) for j in range(i+1,n)])]
+
+  // n = 8
+  // [p for p in permutations(range(n))
+  // if all(
+  // [p[i] != (p[j] - (i-j)) and p[i] != (p[j] - (j-i)) for i in range(n) for j in range(n) if i != j])]
+
+  //[p for p in permutations(range(4))]
+  // [(0, 1, 2, 3), (0, 1, 3, 2), (0, 2, 1, 3), (0, 2, 3, 1), (0, 3, 1, 2), (0, 3, 2, 1), (1, 0, 2, 3), (1, 0, 3, 2), (1, 2, 0, 3), (1, 2, 3, 0), (1, 3, 0, 2), (1, 3, 2, 0), (2, 0, 1, 3), (2, 0, 3, 1), (2, 1, 0, 3), (2, 1, 3, 0), (2, 3, 0, 1), (2, 3, 1, 0), (3, 0, 1, 2), (3, 0, 2, 1), (3, 1, 0, 2), (3, 1, 2, 0), (3, 2, 0, 1), (3, 2, 1, 0)]
+  // permute([0,1,2,3])
+  //object with digits and their distance to current stack
+  // [0, 1, 2, 3]
+  // 1: (4) [1, 0, 2, 3]
+  // 2: (4) [2, 0, 1, 3]
+  // 3: (4) [0, 2, 1, 3]
+  // 4: (4) [1, 2, 0, 3]
+  // 5: (4) [2, 1, 0, 3]
+  // 6: (4) [3, 1, 0, 2]
+  // 7: (4) [1, 3, 0, 2]
+  // 8: (4) [0, 3, 1, 2]
+  // 9: (4) [3, 0, 1, 2]
+  // 10: (4) [1, 0, 3, 2]
+  // 11: (4) [0, 1, 3, 2]
+  // 12: (4) [0, 2, 3, 1]
+  // 13: (4) [2, 0, 3, 1]
+  // 14: (4) [3, 0, 2, 1]
+  // 15: (4) [0, 3, 2, 1]
+  // 16: (4) [2, 3, 0, 1]
+  // 17: (4) [3, 2, 0, 1]
+  // 18: (4) [3, 2, 1, 0]
+  // 19: (4) [2, 3, 1, 0]
+  // 20: (4) [1, 3, 2, 0]
+  // 21: (4) [3, 1, 2, 0]
+  // 22: (4) [2, 1, 3, 0]
+  // 23: (4) [1, 2, 3, 0]
+
+  //while we have unused digits
+  //add lowest unused digit less than n and greater than digit we're replacing
+  //recurse
+
+  //if no unused digits
+  //if digits.length === n
+  //if valid solution
+  //increment solution
+  // return
+
+
+
+
+  /*
   var solutionCount = 0;
   var columnIndexes = new BaseNNumber(n);
   columnIndexes.setLength(n);
@@ -146,7 +238,7 @@ window.countNQueensSolutions = function (n) {
       solutionCount++;
     }
   }
-
+  */
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
