d4a90ca062dd1ad20b5036e68e3e3d480d5866c2
Josh Parker <joshBBparker@gmail.com>
2020-11-14 12:39:05 -0700

    faster find solution

--- src/solvers.js
+++ src/solvers.js
@@ -42,44 +42,41 @@ window.countNRooksSolutions = function (n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function (n) {
-  if (n === 2 || n === 3) {
-    return new Board({ n: n }).rows();
-  }
+  if (n < 4) {
+    var board = new Board({ n: n });
 
-  var columnsIndexes = new Array(n).fill(0);
-  var board = new Board({ n: n });
-  var conflict;
-
-  for (let row = 0; row < n; row++) {
-    while (columnsIndexes[row] < n) {
-      board.togglePiece(row, columnsIndexes[row]);
-      conflict = board.hasAnyQueensConflicts();
-      columnsIndexes[row]++;
-      if (conflict) {
-        board.togglePiece(row, (columnsIndexes[row] - 1));
-      } else {
-        break;
-      }
+    if (n === 1) {
+      board.togglePiece(0, 0);
     }
 
-    if (columnsIndexes[row] === n && conflict) {
-      columnsIndexes[row] = 0;
-      board.togglePiece(row - 1, columnsIndexes[row - 1] - 1);
-      row -= 2;
-    }
+    return board.rows();
   }
 
-  // [1,4,2,4]
-  //
-  // [x, , , ]
-  // [ , , ,x]
-  // [ , , , ]
-  // [ , , , ]
+  var digits = [];
+  var depth = 0;
+  window.finalResult = false;
 
-  var solution = board.rows();
+  var solution = getNextPiece(digits, 0, n, digits => {
+    finalResult = _.clone(digits);
+    console.log('final result: ' + finalResult);
+    return finalResult;
+  }, finalResult);
+
+  console.log(finalResult);
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  return digitsToBoard(finalResult).rows();
+};
+
+window.digitsToBoard = function(digits) {
+  console.log('digits to board: ' + digits);
+  var board = new Board({ n: digits.length });
+
+  for (let i = 0; i < digits.length; i++) {
+    board.togglePiece(i, digits[i]);
+  }
+
+  return board;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
@@ -94,84 +91,66 @@ window.rotateBoard180 = function (digits) {
   });
 };
 
-window.countNQueensSolutions = function (n) {
+window.nextPromisingNumber = function (minimum, digits, depth, n) {
+  if (minimum === undefined) {
+    minimum = 0;
+  }
 
+  var diagonal1 = digits.map((e, i) => e + depth - i);
+  var diagonal2 = digits.map((e, i) => e - depth + i);
+
+  var promising = minimum;
+  do {
+    promising++;
+  } while (digits.includes(promising) || diagonal1.includes(promising) || diagonal2.includes(promising));
+
+  if (promising < n) {
+    return promising;
+  }
+
+  return undefined;
+};
+
+window.getNextPiece = function (digits, depth, n, cb, finalResult) {
+  if (depth === n) {
+    return cb(digits);
+  }
+
+  if (finalResult) {
+    return finalResult;
+  }
+
+  if (digits[0] >= Math.ceil(n / 2)) {
+    return;
+  }
+
+  digits[depth] = -1;
+  while (digits[depth] !== undefined && digits[depth] < n) {
+    digits[depth] = nextPromisingNumber(digits[depth], _.clone(digits.slice(0, digits.length - 1)), depth, n);
+    if (digits[depth] !== undefined && digits[depth] < n) {
+      getNextPiece(_.clone(digits), depth + 1, n, cb);
+    }
+  }
+};
+
+
+
+window.countNQueensSolutions = function (n) {
   if (n < 2) {
     return 1;
   }
 
   var solutionCount = 0;
   var digits = [];
-  var solutions = [];
   var depth = 0;
+  var finalResult = false;
 
-  var nextPromisingNumber = function (minimum, digits, depth) {
-    if (minimum === undefined) {
-      minimum = 0;
-    }
-
-    var diagonal1 = digits.map((e, i) => e + depth - i);
-    var diagonal2 = digits.map((e, i) => e - depth + i);
-
-    var promising = minimum;
-    do {
-      promising++;
-    } while (digits.includes(promising) || diagonal1.includes(promising) || diagonal2.includes(promising));
-
-    if (promising < n) {
-      return promising;
-    }
-
-    return undefined;
-  };
-
-  var getNextPiece = function (digits, depth) {
-    if (depth === n) {
-      console.log('Solution!: ' + digits);
-      // solutions.push(_.clone(digits));
+  getNextPiece(digits, 0, n, () => {
+    solutionCount++;
+    if (digits[0] < Math.floor(n / 2)) {
       solutionCount++;
-      // n: 4 double count if digits[0] is 1 or less
-      // n: 5 double count if digits[0] is 1 or less
-      // n: 6 double count if digits[0] is 2 or less
-      if (digits[0] < Math.floor(n / 2)) {
-        solutionCount++;
-      }
-
-      return;
     }
-
-    if (digits[0] >= Math.ceil(n / 2)) {
-      return;
-    }
-
-    digits[depth] = -1;
-    while (digits[depth] !== undefined && digits[depth] < n) {
-      digits[depth] = nextPromisingNumber(digits[depth], _.clone(digits.slice(0, digits.length - 1)), depth);
-      if (digits[depth] !== undefined && digits[depth] < n) {
-        getNextPiece(_.clone(digits), depth + 1);
-      }
-    }
-  };
-
-  getNextPiece(digits, 0);
-
-  // var stringSolutions = solutions.map(e => e.join(''));
-
-  // solutionCount = stringSolutions.reduce(function (acc, e, i) {
-  //   console.log(stringSolutions);
-  //   var reversedSolution = e.split('').reverse().join('');
-  //   console.log(reversedSolution + ' reversed');
-  //   if (stringSolutions.indexOf(e.toString()) !== i) {
-  //     return acc;
-  //   }
-  //   if (stringSolutions.includes(reversedSolution)) {
-  //     return acc + 1;
-  //   } else {
-  //     return acc + 2;
-  //   }
-  // }, 0);
-
-  // console.log(solutions);
+  }, finalResult);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
